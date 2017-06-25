8f31950fe4a3b5eb124f29ec1fb1c3a2a328227c
leiguoyang <guoyangl@126.com>
2017-06-25 14:52:56 +0800

    Create vehicle-class.rb

---
+++ chapter-3-inheritance/vehicle-class.rb
@@ -0,0 +1,42 @@
+class Vehicle
+  attr_accessor :odometer, :gas_used
+
+  def accelerate
+    puts "Floor it!"
+  end
+  
+  def sound_horn
+    puts "Beep! Beep!"
+  end
+  
+  def steer
+    puts "Turn front 2 wheels."
+  end
+  
+  def mileage
+    @odometer / @gas_used
+  end
+end
+
+# create Car, Truck and Motorcycle class
+class Car < Vehicle
+end
+
+class Truck < Vehicle
+end
+
+class Motorcycle < Vehicle
+end
+
+# create some instances
+
+truck = Truck.new
+truck.accelerate
+truck.steer
+
+car + Car.new
+car.odometer = 11432
+car.gas_used = 366
+
+puts "Lifetime MPG:"
+puts car.mileage
