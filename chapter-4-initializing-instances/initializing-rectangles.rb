23ae06e694ede0b3e1aefbd1bb703f26229b145f
leiguoyang <guoyangl@126.com>
2017-06-26 19:52:38 +0800

    Create new file

---
+++ chapter-4-initializing-instances/initializing-rectangles.rb
@@ -0,0 +1,28 @@
+class Rectangle
+  attr_reader :width, :height
+  
+  def width=(width)
+    if width < 0
+      raise "No negative value is allowed!"
+    else
+      @width = width
+    end
+  end
+  
+  def height=(height)
+    if height < 0
+      raise "No negative value is allowed!"
+    else
+      @height = height
+    end
+  end
+
+  def initialize(width, height)
+    self.width = width
+    self.height = height
+  end
+  
+  def area
+    @width * @height
+  end
+end
