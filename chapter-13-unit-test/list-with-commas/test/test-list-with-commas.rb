af106cf4c3709a116e1877680ffbb30d368282d5
leiguoyang <guoyangl@126.com>
2017-07-13 20:18:25 +0800

    Add chapter-13-unit-test

---
+++ chapter-13-unit-test/list-with-commas/test/test-list-with-commas.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "list-with-commas"
+
+class TestListWithCommas < Minitest::Test
+  def test_it_joins_two_words_with_and
+    list = ListWithCommas.new
+    list.items =["apple", "orange"]
+    assert("apple and orange" == list.join)
+  end
+  
+  def test_it_joins_with_three_words_with_commas
+    list = ListWithCommas.new
+    list.items = ["apple", "orange", "pear"]
+    assert("apple, orange, and pear" == list.join)
+  end
+end
